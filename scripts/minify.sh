a478d1c95284b42d42a0310f927f2d5a000a039c
fluffy-momonga <fluffy.momonga+github.637cabe9@gmail.com>
2023-03-01 17:34:21 +0900

    source map

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -13,12 +13,12 @@ cleancss --source-map -o style.min.css style.css
 cd ..
 
 cd js
-uglifyjs service.js -m -c --source-map -o service.min.js
-uglifyjs annict.js -m -c reduce_vars=false --source-map -o annict.min.js
-uglifyjs dialog.js -m -c --source-map -o dialog.min.js
-uglifyjs header.js -m -c --source-map -o header.min.js
-uglifyjs watching.js -m -c --source-map -o watching.min.js
-uglifyjs search.js -m -c --source-map -o search.min.js
+uglifyjs service.js -m -c --source-map "url='service.min.js.map'" -o service.min.js
+uglifyjs annict.js -m -c reduce_vars=false --source-map "url='annict.min.js.map'" -o annict.min.js
+uglifyjs dialog.js -m -c --source-map "url='dialog.min.js.map'" -o dialog.min.js
+uglifyjs header.js -m -c --source-map "url='header.min.js.map'" -o header.min.js
+uglifyjs watching.js -m -c --source-map "url='watching.min.js.map'" -o watching.min.js
+uglifyjs search.js -m -c --source-map "url='search.min.js.map'" -o search.min.js
 cd ..
 
 html-minifier --collapse-whitespace index.html -o index.min.html
